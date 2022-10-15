572073141eedad5a9e11160b0aab9d6c0c2cae20
Valiaaa <valialcc@gmail.com>
2022-10-14 22:26:27 -0400

    update ticket page

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ $(document).ready(function(){
     $('.list').click(function(){
         $(".custom-select").removeAttr("open");
     });
+
+    $('.list2').click(function(){
+        $(".custom-select2").removeAttr("open");
+    });
     
 });
