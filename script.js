50783098206e637768d9a8d3b05d222f7e58ac31
Valiaaa <valialcc@gmail.com>
2022-10-21 14:43:05 -0400

    update adaptation

--- script.js
+++ script.js
@@ -7,5 +7,16 @@ $(document).ready(function(){
     $('.list2').click(function(){
         $(".custom-select2").removeAttr("open");
     });
+
+    $(window).resize(function() {
+        if ($(window).width() < 550) {
+            $("#general").html("GENERAL<br>ADMISSION")
+            $("#general").css("line-height","26px")
+            $(".custom-select2").css("transform","translateY(-117px)")
+        }
+        else {
+            $("#general").html("GENERAL ADMISSION")
+        }
+    });
     
 });
